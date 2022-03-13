ccdb26a09237e0ccb11bf81ddb673fc4ae781dda
LopoBoco <rol-ex9@yandex.ru>
2022-03-13 22:05:11 +0300

    My

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+
+    private static UserServiceImpl userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        userService.createUsersTable();
+
+        userService.saveUser("Алексей", "Алексеев", (byte) 25);
+        userService.saveUser("Петр", "Петров", (byte) 31);
+        userService.saveUser("Иван", "Иванов", (byte) 40);
+        userService.saveUser("Павел", "Павлов", (byte) 22);
+
+        userService.removeUserById(3);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,15 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String url = "jdbc:mysql://localhost:3306/mysql?useSSL=false";
+    private static final String userName = "root";
+    private static final String password = "root";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, userName, password);
+    }
 }
